175a85b34429c4722c519f62cf140e60d8fea8e7
jhzong <jhzong05@gmail.com>
2026-01-30 12:41:16 +0900

    제너릭스(generics)

---
+++ j0130/src/j0130/C04.java
@@ -0,0 +1,52 @@
+package j0130;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class C04 {
+
+	public static void main(String[] args) {
+		ArrayList<Card> list = new ArrayList();
+		Card c1 = new Card("SPADE",1);
+		Card c2 = new Card("SPADE",2);
+		Card c3 = new Card("SPADE",3);
+		Stuscore s1 = new Stuscore("홍길동", 100, 100, 100);
+		Stuscore s2 = new Stuscore("유관순", 100, 100, 100);
+		Stuscore s3 = new Stuscore("이순신", 100, 100, 100);
+		String str1 = "홍길자";
+		String str2 = "홍길순";
+		list.add(c1);
+		list.add(c2);
+		list.add(c3);
+//		list.add(s1);
+//		list.add(s2);
+//		list.add(s3);
+//		list.add(str1);
+//		list.add(str2);
+		
+		// 출력(generics)
+		for(int i=0;i<list.size();i++) {
+			Card c = list.get(i);//형변환생략
+			System.out.println(c.kind);
+		}
+		
+		
+		// 출력(instanceof)
+//		for(int i=0;i<list.size();i++) {
+//			if (list.get(i) instanceof Card) {
+//				Card c = (Card)list.get(i);
+//				System.out.println(c.kind+","+c.number);
+//			}else if (list.get(i) instanceof Stuscore) {
+//				Stuscore s = (Stuscore)list.get(i);
+//				System.out.println(s.getName());
+//			}else if (list.get(i) instanceof String) {
+//				String s = (String)list.get(i);
+//				System.out.println(s);
+//			}
+//		}
+//		
+//		System.out.println(list);
+	}
+
+}
---
+++ j0130/src/j0130/Card.java
@@ -0,0 +1,14 @@
+package j0130;
+
+public class Card {
+	
+	Card() {}
+	Card(String kind,int number){
+		this.kind = kind;
+		this.number = number;
+	}
+	
+	int number;
+	String kind;
+	
+}
