8d5d53835c1b370ed9ca54323a25653910ba4817
jhzong <jhzong05@gmail.com>
2026-01-30 18:09:29 +0900

    java 파일읽기/저장

--- j0130/src/j0130/C07.java
+++ j0130/src/j0130/C07.java
@@ -7,10 +7,10 @@ import java.io.IOException;
 public class C07 {
 
 	public static void main(String[] args) {
-		String filrPath = "c:/aaa/a1.txt";
+		String filePath = "c:/aaa/a1.txt";
 		try {
 			// 설정이 없으면:덮어쓰기, true:
-			FileWriter fw = new FileWriter(filrPath,true);
+			FileWriter fw = new FileWriter(filePath,true);
 			BufferedWriter bw = new BufferedWriter(fw);
 			String txt = "Hello World!!\r\n";// \r:줄의 제일 끝으로 이동,\n:엔터키
 			bw.write(txt);
